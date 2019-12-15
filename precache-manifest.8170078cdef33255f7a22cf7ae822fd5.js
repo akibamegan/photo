264bd0244e0d8daa6406203ba47e25e468f84ca0
Megan Akiba <megan.akiba@colorado.edu>
2019-12-14 20:51:08 -0700

    Updates

---
+++ precache-manifest.8170078cdef33255f7a22cf7ae822fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d3d82aec1158735796a02205a4c83a",
+    "url": "/photo/index.html"
+  },
+  {
+    "revision": "4f9cc17c31eafd1aec5c",
+    "url": "/photo/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "a6bee4e02c00a9abf304",
+    "url": "/photo/static/css/main.9dcc3ef7.chunk.css"
+  },
+  {
+    "revision": "4f9cc17c31eafd1aec5c",
+    "url": "/photo/static/js/2.e4082abf.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/photo/static/js/2.e4082abf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6bee4e02c00a9abf304",
+    "url": "/photo/static/js/main.2b43ccbc.chunk.js"
+  },
+  {
+    "revision": "5febd1e93007443eaba2",
+    "url": "/photo/static/js/runtime-main.29b46a9b.js"
+  }
+]);
